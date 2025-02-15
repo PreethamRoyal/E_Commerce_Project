9e2e95f0279c4a6f9643f917e5ddfaef755917c1
Preetham <preetham.sb700@gmail.com>
2025-02-15 17:58:41 +0530

    Integration Of Template

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
-  
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  templateUrl: './product-list-grid.component.html',
+  //templateUrl: './product-list.component.html',
+  //templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
